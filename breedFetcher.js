73f29fbbdb4164c4b3f8e08c657c2345ce49783e
Josh Sarnecki <joshsarnecki@gmail.com>
2022-02-17 23:20:09 +0000

    Cleaned up code and added comments

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,33 +1,29 @@
 const request = require("request");
-
 const args = process.argv.slice(2);
+  //Allow for CLI args, with first arg being name
+  //and return descripttion for that name
 
 const catFirstLetters = args[0].slice(0, 4);
+//In the case of names being too long or two words, slice the first few characters to search on API
 
 request(`https://api.thecatapi.com/v1/breeds/search?q=${catFirstLetters}`, (error, response, body) => {
   
   if (error) {
     console.log("error: ", error);
   }
- 
-  // console.log("response: ", response[0]);
 
   const data = JSON.parse(body);
-  //console.log("data: ", data);
+  //Turns raw data into JSON to be manipulated
 
   if (data[0] === undefined) {
+    //Prints message if database does not find info on cat input
     console.log(`Sorry, unable to find information on ${args[0]}`);
     process.exit();
   }
-  //console.log(`${args[0]}: ${data}`)
 
   console.log(`${args[0]} Breed description: `, data[0].description);
-  //console.log(" breed: ", data["American Curl"]);  //The data is an array of objects, and each obj is a different breed
-  //console.log("data type: ", typeof data);
+  //If the search finds the input, it will print the description
 
-
-
-  //Allow for CLI args, with first arg being name
-
-  //and return descripttion for that name
-})
+  ///Notes:
+  ////The data is an array of objects, and each obj is a different breed
+});
